a534d9709c205b57dcf6bca23b75ccefc9f6b495
Rira Choi <rira.nunu.choi@gmail.com>
2014-12-03 10:21:23 +1100

    cleaned up

--- table_manager.rb
+++ table_manager.rb
@@ -4,6 +4,7 @@ class TableManager
 
   attr_accessor :news_data
 
+### Initialize - Creating empty Hashes
   def initialize(news_data=nil)
     @news_data = {}
     @news_data[:banner] = {}
@@ -11,7 +12,7 @@ class TableManager
     @news_data[:shopping_cta] = {}
   end
 
-#### CSV : convert data to HTML <a>tag with attributes
+### CSV : convert data to HTML <a>tag with attributes
   
   # Open CSV File and Save @content without empty cells
   def open_csv(file_name)
@@ -30,19 +31,19 @@ class TableManager
     end
   end 
   
-  #Filtering the main banner, top picks and shopping cta
+  # Filtering the main banner, top picks and shopping cta
   def filtering_links      
     @content.each_with_index do |c, i|
-      # Validate URL
+      # Validate URL - Only for CTA because the product links are picked up from the website
       if !c[:cta].nil? && (c[:cta_link]).split(/\./).first != "http://www" 
-        invalid_url = (c[:cta_link]).split(/\./)[1..-1] # If there is no http:// - start with only 'www.'
+        invalid_url = (c[:cta_link]).split(/\./)[1..-1] # If there is no http:// - When start with only 'www.'
         c[:cta_link] = invalid_url.unshift("http://www").join('.')
       else
         c[:cta_link]
       end 
 
-      #HASH FORMAT( title[link] )
-      if !c[:cta].nil? && i < 4 # The main banner will be within indext of 3.
+      # HASH FORMAT( title[link] )
+      if !c[:cta].nil? && i < 4 # The main banner will be within index of 3.
         @news_data[:banner][c[:cta]] = c[:cta_link]
       elsif !c[:product_details].nil? # The top picks that have product details.
         @news_data[:top_picks][c[:product_details]] = c[:product_links]
@@ -56,53 +57,40 @@ class TableManager
     # p @news_data[:shopping_cta]      
   end
 
-  def grab_links(data)
-    @data = data
-    bannerHTML = '' 
-    banners = @data[:banner] 
-    top_picks = @data[:top_picks] # from the second element to the last 
+  def grab_links(news_data)
+    @news_data = news_data 
+    banners = @news_data[:banner] 
+    top_picks = @news_data[:top_picks]
+    shopping_ctas = @news_data[:shopping_cta]
 
-    # banners HTML
-    @html_banners = []
-    banners.each do |b|
-      if b[:cta_link] != nil 
-        banner_html = "<a href='#{ b[:cta_link]}' title='#{b[:cta]}' alt='#{b[:cta]}' target='_blank'>"     
-        @html_banners << banner_html
-      else
-        p "There is no banner links for BANNERS"
-      end 
+    # Banner's HTML
+    banners.each do |banner|
+      @title = banner.shift
+      @href_links = banner.pop
+      "<a href=\"#{ @href_links }\" target=\"_blank\" title=\"#{ @title }\">"
     end 
 
-    # top picks HTML
-    @html_top_picks = []
-    top_picks.each do |t|
-      # Product for top picks 
-      if t[:product_links] != nil
-        product_html = "<a href='#{ t[:product_links]}' title='#{t[:product_details]}' alt='#{t[:product_details]}' target='_blank'>"
-        @html_top_picks << product_html
-      else
-        "There is no product links for TOP PICKS"      
-      end 
-      # CTA for top picks
-      if t[:cta_link] != nil 
-        pro_cta_html = "<a href='#{ t[:cta_link]}' title='#{t[:cta]}' alt='#{t[:cta]}' target='_blank'>"
-        
-        @html_top_picks << pro_cta_html      
-      else
-        "There is no cta links for TOP PICKS"
-      end  
+    # Top Picks's HTML
+    top_picks.each do |top_pick|
+      @title = top_pick.shift
+      @href_links = top_pick.pop
+      "<a href=\"#{ @href_links }\" target=\"_blank\" title=\"#{ @title }\">"
     end 
 
-    ## TRY TO PRINT THEM HERE,
-    @html_banners
-    @html_top_picks
-    puts 'This is banners,' 
-    puts @html_banners
-    puts 'This is top_picks,'
-    puts @html_top_picks
+    # Shopping CTA's HTML
+    shopping_ctas.each do |shopping_cta|
+      @title = shopping_cta.shift
+      @href_links = shopping_cta.pop
+      "<a href=\"#{ @href_links }\" target=\"_blank\" title=\"#{ @title }\">"
+    end 
+    p banners
+    p top_picks
+    p shopping_ctas
+
+
   end
 
-#### HTML
+### HTML - Insert code for Email. 
   def html_with_style(htmlfile)
     infile = htmlfile
     outfile = "test_output.html"
@@ -121,20 +109,29 @@ class TableManager
     File.open(outfile, "w") {|out| out << new_contents }
   end
 
-  def insert_links(file_name)
-
+  def insert_links(output_file, news_data)
+    @output_file = output_file
+    @news_data = news_data
   end 
 
 end
 
 
-#TESTING
+### TESTING
 
-
+# Initialize
 t1 = TableManager.new
-t1.open_csv('email_test.csv')
+
+# Open CSV file
+the_csv_file = Dir['*'].select {|x| x =~ /_.*(csv)/ }.sort.first
+t1.open_csv(the_csv_file)
+
+# Filter data and Convert data to <a>tag
 t1.filtering_links
-# t1.grab_links(t1.news_data)
-# Grab input html file
+t1.grab_links(t1.news_data)
+
+# Open HTML file(input) and wirte new HTML file(output)
 the_input_file = Dir['*'].select {|x| x =~ /_.*(html)/ }.sort.first
 t1.html_with_style(the_input_file)
+
+
