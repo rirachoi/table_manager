898661119284ba1e3f0c4913be5bbc6d9244fe55
Rira Choi <rira.nunu.choi@gmail.com>
2014-12-09 16:48:36 +1100

    yo happpppy yo

--- table_manager.rb
+++ table_manager.rb
@@ -11,22 +11,20 @@ class TableManager
   end
 
 ### CSV : convert data to HTML <a>tag with attributes  
-  # Open CSV File and Save @content without empty cells
+ # Open CSV File and Save @content without empty cells
   def open_csv(file_name)
     CSV.foreach(file_name,
                 :headers => true,
                 :converters => :all,
                 :header_converters => :symbol
                 ) do |row|
-      # Make CSV data as hash
-      hash_row = row.to_hash
+      hash_row = row.to_hash # Make CSV data as hash
       # Get rid of blank cells - Putting all data into @content
       if hash_row[:product_details] != nil || hash_row[:cta_link] != nil
         @content << hash_row
       end
     end
   end 
-
 
   # Filtering links
   def filtering_links     
@@ -39,8 +37,6 @@ class TableManager
         c[:cta_link]
       end 
     end
-    # puts @content
-
   end
 
 ### HTML - Insert code for Email. 
@@ -49,44 +45,33 @@ class TableManager
     outfile = "test_output.html"
 
     text = File.read(htmlfile)
-
     # TD Style
     new_contents = text.gsub(/(<td)([.\s\S]*?)>([.\s\S]*?<img[.\s\S]*?width[:=]"?)(\d+)([.\s\S]*?height[:=]"?)(\d+)/, '\\1 width="\\4" height="\\6"\\2 style="font-size: 8px; min-width:\\4px;">\\3\\4\\5\\6')
     new_contents = new_contents.gsub(/<\/td>/, "\n\t\t<\/td>")
-
     # IMG Style
     new_contents = new_contents.gsub(/<img/, "<img style='display: block; border: 0px; font-size: 8px;'")
-
     # TR SPACER GIF Style 
     new_contents = new_contents.gsub('\' src="images/spacer.gif"', ' -webkit-text-size-adjust: none !important; -moz-text-size-adjust:none !important;\' src="images/spacer.gif"')
-
-    # TEST OUTPUT html
+    # Wrting the result
     File.open(outfile, "w") {|out| out << new_contents } # No need to close the file when using the block for File class.
   end
 
-  # Insert links - with VOUCHER CODE version
+  # Insert links - with VOUCHER CODE version - Matching alt attributes(from Photoshop) with @title(from CSV)
   def insert_links_with_voucher(output_file)
-  ## Matching alt tag(from Photoshop) with @title(from CSV)
     html_string = File.read(output_file)
-
     # Inserting '#' links when the <img> has alt tag.
     html_string = html_string.gsub(/(<img.*alt=")(.+)(">)/, '<a href="#" target="_blank">\\1\\2" title="\\2\\3</a>')
-
-    # Inserting products links
-    doc = Nokogiri::HTML(html_string)
-
-    # alt_string = doc.xpath("//a//img//@alt")
-
     ## With this code, the links in CSV must be ordered (same as photoshop slices)
-    doc.xpath("//a").each_with_index do |a, i|
+    doc = Nokogiri::HTML(html_string)
+    doc.xpath("//a").each_with_index do |a, i|    
       # Banner CTA (from 0 to 2)
       if i < 2  
         a['href'] = @content[0][:cta_link]
-      # Conditions & Policy 
-      elsif i == 2
+      # Conditions & Policy       
+      elsif i == 2 
         a['href'] = @content[1][:cta_link]
       # Products
-      else
+      else 
         i = i - 1 # It will loop when i = 3 but the third <a> link should be from @content[2]
         next if i > @content.length - 1 or @content[i][:product_links].nil? or !@content[i][:product_links].include? 'http' or !a.xpath("//a//img//@alt") == @content[i][:product_details]
         a['href'] = @content[i][:product_links] 
@@ -95,38 +80,52 @@ class TableManager
 
     # Shopping CTA
     shopping_cta = []
-
-    doc.xpath("//a").each do |c|
+    doc.xpath("//a").each do |c| # grabbing rest of <a> tags, which will be shopping CTAs. 
       shopping_cta << c if c['href'] == "\#"
     end 
-
     shopping_cta.reverse.each_with_index do |s, f| # This array should be reversed.
       f = f + 1 # Shopping CTA should be inserted in the reversed way. 
       s['href'] = @content[-f][:cta_link]
     end 
-
+    # Wrting the result
     File.open(output_file, "w") {|out| out << doc.to_s }
   end 
-
-
 
   # Clean up the html and leave only a single table
   def last_clean_up(output_file)
     text = File.read(output_file)
-
     # MAIN TABLE STYLE
     new_contents = text.gsub(/([.\n\r\s\S]*?)(<table)([.\s\S]*?)(id="Table_01")([.\n\r\s\S]*?)(<\/table>)([.\n\r\s\S]*?<\/html>)/, "<table style='min-width:620px;' align='center'\\5\\6\\3")
-
     # Delete amp; for tracking the URL. 
     new_contents = new_contents.gsub(/amp;/,"")
-
+    # Wrting the result
     File.open(output_file, "w") {|out| out << new_contents }
   end 
 
-end
+  # Insert links - with NORMAL version
+  def insert_links_normal(output_file)
+  ## Matching alt tag(from Photoshop) with @title(from CSV)
+    html_string = File.read(output_file)
+    # Inserting '#' links when the <img> has alt tag.
+    html_string = html_string.gsub(/(<img.*alt=")(.+)(">)/, '<a href="#" target="_blank">\\1\\2" title="\\2\\3</a>')
+  ## With this code, the links in CSV must be ordered (same as photoshop slices)
+    doc = Nokogiri::HTML(html_string)
+    doc.xpath("//a").each_with_index do |a, i|
+      # Products
+      next if i > @content.length - 1 or @content[i][:product_links].nil? or !@content[i][:product_links].include? 'http' or !a.xpath("//a//img//@alt") == @content[i][:product_details]
+        a['href'] = @content[i][:product_links]
+    end
+    # doc.xpath("//a").each_with_index do |a, i|
+    #   # Banner & CTA
+    #   next if i > @content.length - 1 or @content[i][:cta_link].nil? or !@content[i][:cta_link].include? 'http' or !a.xpath("//a//img//@alt") == @content[i][:cta]
+    #     a['href'] = @content[i][:cta_link]
+    # end
+    File.open(output_file, "w") {|out| out << doc.to_s }
+  end 
+end # END OF CLASS / TABLE MANAGER 
 
 
-### TESTING
+######## TESTING
 
 # Initialize
 t1 = TableManager.new
